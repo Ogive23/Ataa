1b4ba53f48112052e715aa05cf3b74c3db7d1e9f
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2020-09-16 21:36:54 +0200

    Added user location

---
+++ lib/Models/user_location.dart
@@ -0,0 +1,22 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class UserLocation{
+  Position currentLocation;
+  LatLng latLng;
+  Future<Position> locateUser() async {
+    return Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+  getUserLocation() async {
+    currentLocation = await locateUser();
+    latLng = LatLng(currentLocation.latitude, currentLocation.longitude);
+    print('center $latLng');
+  }
+  LatLng getLatLng(){
+    return latLng;
+  }
+  updateLatLng(){
+    getUserLocation();
+  }
+}
