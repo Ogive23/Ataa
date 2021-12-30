4176f67230eb603df3fa6ea64499d7f2adb04770
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-30 12:40:16 +0200

    Added Additional Elements to Login API

--- lib/APICallers/UserApiCaller.dart
+++ lib/APICallers/UserApiCaller.dart
@@ -13,13 +13,18 @@ class UserApiCaller {
   SessionManager sessionManager = new SessionManager();
   DataMapper dataMapper = new DataMapper();
   TokenApiCaller tokenApiCaller = new TokenApiCaller();
-  String url = "http://192.168.1.139:8000";
+  String url = "http://192.168.1.136:8000";
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     var headers = {
       "Content-Type": "application/json",
     };
-    var body = {"email": email, "password": password};
+    var body = {
+      "email": email,
+      "password": password,
+      "accessType": "Mobile",
+      "appType": "Ataa"
+    };
     try {
       var response = await http
           .post(Uri.parse(url + "/api/login"),
@@ -44,39 +49,6 @@ class UserApiCaller {
     } catch (e) {
       print('e = $e');
       return responseHandler.errorPrinter("En", 'SomethingWentWrong');
-    }
-  }
-
-  Future<Map<String, dynamic>> getAchievements(String language) async {
-    Map<String, dynamic> status;
-    if (sessionManager.accessTokenExpired()) {
-      status = await tokenApiCaller.refreshAccessToken(language);
-      if (status['Err_Flag']) return status;
-    }
-    var headers = {
-      "Content-Type": "application/json",
-      'Authorization': 'Bearer ${sessionManager.accessToken}',
-    };
-    try {
-      print(url + "/api/ataa/achievement/${sessionManager.user!.id}");
-      var response = await http
-          .get(
-              Uri.parse(
-                  url + "/api/ataa/achievement/${sessionManager.user!.id}?requesterId=${sessionManager.user!.id}"),
-              headers: headers)
-          .catchError((error) {
-        throw error;
-      }).timeout(Duration(seconds: 120));
-      Map<String, dynamic> responseToJson = jsonDecode(response.body);
-      print(responseToJson);
-      return responseToJson;
-    } on TimeoutException {
-      return responseHandler.timeOutPrinter(language);
-    } on SocketException {
-      return responseHandler.errorPrinter(language, "InternetError");
-    } catch (e) {
-      print('e = $e');
-      return responseHandler.errorPrinter(language, "SomethingWentWrong");
     }
   }
 
