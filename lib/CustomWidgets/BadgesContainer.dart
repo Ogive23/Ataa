df2c56682d40143f353dca018ed908065b46bc49
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-01-31 16:07:53 +0200

    Update Badge Container to Show Dynamic Info

--- lib/CustomWidgets/BadgesContainer.dart
+++ lib/CustomWidgets/BadgesContainer.dart
@@ -1,9 +1,12 @@
 import 'package:ataa/APICallers/AchievementApiCaller.dart';
 import 'package:ataa/CustomWidgets/CustomSpacing.dart';
 import 'package:ataa/CustomWidgets/ErrorMessage.dart';
+import 'package:ataa/Helpers/DataMapper.dart';
+import 'package:ataa/Models/Badge.dart';
 import 'package:ataa/Shared%20Data/AppLanguage.dart';
 import 'package:ataa/Shared%20Data/AppTheme.dart';
 import 'package:ataa/Shared%20Data/MemoryCache.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,18 +17,21 @@ class BadgesContainer extends StatelessWidget {
   static late AppLanguage appLanguage;
   final AchievementApiCaller achievementApiCaller = new AchievementApiCaller();
   final MemoryCache memoryCache = new MemoryCache();
-  cacheData(Map<String, dynamic> data) {
-    memoryCache.setData('ataaBadges', data);
+
+  cacheData(List<Badge> badges) {
+    memoryCache.setData('ataaBadges', badges);
   }
 
   @override
   Widget build(BuildContext context) {
+    //ToDo: Remove
+    memoryCache.removeData('ataaBadges');
     w = MediaQuery.of(context).size.width;
     h = MediaQuery.of(context).size.height;
     appTheme = Provider.of<AppTheme>(context);
     appLanguage = Provider.of<AppLanguage>(context);
     return memoryCache.hasData('ataaBadges')
-        ? getSuccessBody(memoryCache.getData('ataaBadges'))
+        ? getSuccessBody(context, memoryCache.getData('ataaBadges'))
         : FutureBuilder<Map<String, dynamic>>(
             future: achievementApiCaller.getAtaaBadges(appLanguage.language),
             builder: (BuildContext context,
@@ -38,8 +44,11 @@ class BadgesContainer extends StatelessWidget {
                     padding: EdgeInsets.symmetric(vertical: h / 100),
                     child: ErrorMessage(message: snapshot.data!['Err_Desc']),
                   );
-                cacheData(snapshot.data!);
-                return getSuccessBody(snapshot.data!);
+                DataMapper dataMapper = new DataMapper();
+                List<Badge> badges =
+                    dataMapper.getBadgesFromJson(snapshot.data!['data']);
+                cacheData(badges);
+                return getSuccessBody(context, badges);
               } else if (snapshot.error != null) {
                 return Container(
                   alignment: Alignment.center,
@@ -56,7 +65,7 @@ class BadgesContainer extends StatelessWidget {
                     ),
                     CustomSpacing(value: 50),
                     Text(
-                      'جاري تحميل الجوائز',
+                      'جاري تحميل الشارات',
                       style: appTheme.themeData.primaryTextTheme.headline3,
                     )
                   ],
@@ -66,12 +75,12 @@ class BadgesContainer extends StatelessWidget {
           );
   }
 
-  getSuccessBody(Map<String, dynamic> badges) {
-    switch (badges['data'].length) {
+  getSuccessBody(BuildContext context, List<Badge> badges) {
+    switch (badges.length) {
       case 0:
         return Center(
           child: Text(
-            'لا توجد جوائز حالياً',
+            'لا توجد شارات حالياً',
             style: appTheme.themeData.primaryTextTheme.headline3,
           ),
         );
@@ -79,43 +88,102 @@ class BadgesContainer extends StatelessWidget {
         return GridView.count(
           crossAxisCount: 2,
           addRepaintBoundaries: true,
-          // childAspectRatio: 0.5,
-          children: [1, 2, 3, 4, 5, 6, 7, 8]
-              .map((e) => Card(
-                    shape: Border.all(),
-                    elevation: 1.0,
-                    color: Colors.transparent,
-                    shadowColor: appTheme.themeData.shadowColor,
-                    // margin: EdgeInsets.symmetric(
-                    //     vertical: h / 100, horizontal: w / 50),
-                    child: Container(
-                      height: h / 5,
-                      padding: EdgeInsets.symmetric(
-                          vertical: h / 100, horizontal: w / 50),
-                      // color: Color.fromRGBO(20, 32, 67, 1.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Level ' + e.toString(),
-                            style:
-                                appTheme.themeData.primaryTextTheme.headline6,
-                          ),
-                          Image.asset(
-                            'assets/images/winner-trophy-cup-prize-award-best-first-achievement-29309.png',
-                            height: h / 6,
-                          ),
-                          Text(
-                            'Completed',
-                            style: appTheme
-                                .themeData.primaryTextTheme.headline3!
-                                .apply(color: Colors.amber),
-                          )
-                        ],
+          children: badges
+              .map((badge) => GestureDetector(
+                    onTap: () {
+                      showBadgeDialog(context, badge);
+                    },
+                    child: Card(
+                      shape: Border.all(),
+                      elevation: 1.0,
+                      color: Colors.transparent,
+                      shadowColor: appTheme.themeData.shadowColor,
+                      child: Container(
+                        height: h / 5,
+                        padding: EdgeInsets.symmetric(
+                            vertical: h / 100, horizontal: w / 50),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              badge.name.toString(),
+                              style:
+                                  appTheme.themeData.primaryTextTheme.headline6,
+                            ),
+                            CachedNetworkImage(
+                              imageUrl: badge.image,
+                              height: h / 7,
+                              color: badge.acquired
+                                  ? null
+                                  : Colors.transparent.withOpacity(0.3),
+                            ),
+                            Text(
+                              badge.active
+                                  ? badge.acquired
+                                      ? 'Acquired'
+                                      : 'On going'
+                                  : 'Inactive',
+                              style: appTheme
+                                  .themeData.primaryTextTheme.headline3!
+                                  .apply(color: Colors.amber),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ))
               .toList(),
         );
     }
+  }
+
+  Future<void> showBadgeDialog(context, Badge badge) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return Directionality(
+          textDirection: appLanguage.textDirection,
+          child: AlertDialog(
+            backgroundColor: appTheme.themeData.primaryColor,
+            insetPadding:
+                EdgeInsets.symmetric(horizontal: w / 20, vertical: h / 10),
+            contentPadding: EdgeInsets.zero,
+            title: Text(
+              badge.name,
+              style: appTheme.themeData.primaryTextTheme.headline4,
+            ),
+            content: Container(
+              width: double.maxFinite,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    CachedNetworkImage(
+                      imageUrl: badge.image,
+                      height: h / 6,
+                      color:
+                          badge.acquired ? null : Colors.white.withOpacity(0.8),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text("حسناً",
+                    textDirection: appLanguage.textDirection,
+                    style: appTheme.themeData.primaryTextTheme.headline4),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        appTheme.themeData.accentColor)),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
---
+++ lib/Models/Badge.dart
@@ -0,0 +1,19 @@
+class Badge {
+  String id;
+  String name;
+  String arabicName;
+  String description;
+  String image;
+  bool active;
+  bool acquired;
+  DateTime? acquiredAt;
+  Badge(
+      {required this.id,
+      required this.name,
+      required this.arabicName,
+      required this.description,
+      required this.image,
+      required this.active,
+      required this.acquired,
+      this.acquiredAt});
+}
