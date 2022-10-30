6c2524bf975b84af861418f57611f3d658bd0eab
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-10-30 05:23:09 +0200

    Udpated Error Message to Get App Param

--- lib/CustomWidgets/ErrorMessage.dart
+++ lib/CustomWidgets/ErrorMessage.dart
@@ -8,33 +8,35 @@ import 'package:provider/provider.dart';
 
 class ErrorMessage extends StatelessWidget {
   final String message;
-  static late AppTheme appTheme;
-  static late AppLanguage appLanguage;
+  AppTheme? appTheme;
+  AppLanguage? appLanguage;
   static late double h;
-  const ErrorMessage({Key? key, required this.message}) : super(key: key);
+  ErrorMessage({Key? key, required this.message, this.appTheme, this.appLanguage})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    appTheme = Provider.of<AppTheme>(context);
-    appLanguage = Provider.of<AppLanguage>(context);
+    appTheme ??= Provider.of<AppTheme>(context);
+    appLanguage ??= Provider.of<AppLanguage>(context);
     h = MediaQuery.of(context).size.height;
     return Container(
         alignment: Alignment.center,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Lottie.asset('assets/animations/38213-error.json', height: h / 10),
             Text(
               message,
-              style: appTheme.nonStaticGetTextStyle(
+              style: appTheme!.nonStaticGetTextStyle(
                   1.0,
                   Colors.red[300],
-                  appTheme.mediumTextSize(context),
+                  appTheme!.mediumTextSize(context),
                   FontWeight.normal,
                   1.0,
                   TextDecoration.none,
                   'OpenSans'),
               textAlign: TextAlign.center,
-              textDirection: appLanguage.textDirection,
+              textDirection: appLanguage!.textDirection,
             ),
           ],
         ));
